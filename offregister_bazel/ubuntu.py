68a4ccdcfd4661f6f8126e68a0a8fe81b6fb59cf
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-08-25 22:06:30 -0400

    [*.py] Update to threadsafe Python 3 compatible Fabric ; [README.md] Update support Python version range ; [LICENSE-{APACHE,MIT}] Update copyright year range

--- offregister_bazel/ubuntu.py
+++ offregister_bazel/ubuntu.py
@@ -1,21 +1,20 @@
-from fabric.api import sudo
-from fabric.contrib.files import append
 from offregister_fab_utils.apt import apt_depends
 from offregister_fab_utils.fs import cmd_avail
+from patchwork.files import append
 
 
 def install_bazel(from_ppa=True):
     if not from_ppa:
         raise NotImplementedError()
-    elif cmd_avail("bazel"):
+    elif cmd_avail(c, "bazel"):
         return
 
     fname = "/etc/apt/sources.list.d/bazel.list"
-    sudo("touch {}".format(fname))
+    c.sudo("touch {}".format(fname))
     append(
         filename=fname,
         text="deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8",
         use_sudo=True,
     )
-    sudo("curl https://bazel.build/bazel-release.pub.gpg | apt-key add -")
-    apt_depends("openjdk-8-jdk", "bazel")
+    c.sudo("curl https://bazel.build/bazel-release.pub.gpg | apt-key add -")
+    apt_depends(c, "openjdk-8-jdk", "bazel")
